89b15510112ecf63ab99193462453108846e95d6
Gideon kirui <kipyegongideon99@gmail.com>
2024-03-20 16:21:55 +0300

    arrow function

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function() {
+ return 2000 / 100;   
+}
+const square = (parameter1) => parameter1 **2 ;
+square(4);
+
+const add = (parameter1,parameter2) =>parameter1 + parameter2;
+add;
+
+
